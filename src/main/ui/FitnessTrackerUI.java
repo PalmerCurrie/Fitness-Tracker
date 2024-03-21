4f99a5ede3d3f6583ba67b878b6de732b9628dcb
Palmer Currie <pcurri01@students.cs.ubc.ca>
2024-03-20 20:58:41 -0700

    Fixed functionality of ViewWorkouts now updates every time button is clicked. Added functionality to select the workout you want on the button click.

--- src/main/ui/FitnessTrackerUI.java
+++ src/main/ui/FitnessTrackerUI.java
@@ -8,6 +8,7 @@ import persistence.JsonWriter;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.List;
 
@@ -23,6 +24,8 @@ public class FitnessTrackerUI extends JFrame {
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
     private static final String JSON_STORE = "./data/fitnesstracker.json";
+    private DefaultListModel<String> model = new DefaultListModel<>();
+    private JList<String> workoutJList = new JList<>(model);
 
     // Constructor sets up panel
     public FitnessTrackerUI() {
@@ -33,7 +36,7 @@ public class FitnessTrackerUI extends JFrame {
         desktop = new JDesktopPane(); // creates desktop
 
         // creates the control panel
-        controlPanel = new JInternalFrame("Control Panel", true, false, false);
+        controlPanel = new JInternalFrame("Fitness Tracker Panel", true, false, false);
         // For Control Panel
         controlPanel.setPreferredSize(new java.awt.Dimension(200, 300));
         controlPanel.pack();
@@ -41,14 +44,10 @@ public class FitnessTrackerUI extends JFrame {
         desktop.add(controlPanel);
 
 
-
-        // Creates panel for WorkoutList
         workoutListFrame = new JInternalFrame("Workout List", false, false, false);
-        workoutListFrame.setBackground(Color.blue);
-        workoutListFrame.setPreferredSize(new java.awt.Dimension(300, 300));
-        workoutListFrame.pack();
-        workoutListFrame.setVisible(true);
-        desktop.add(workoutListFrame);
+
+
+
 
 
         setContentPane(desktop);
@@ -70,7 +69,7 @@ public class FitnessTrackerUI extends JFrame {
     // Helper method that adds button panel
     public void addButtonPanel() {
         JPanel buttonPanel = new JPanel();
-        buttonPanel.add(new JButton(new HelperButtonClass()));
+        //buttonPanel.add(new JButton(new HelperButtonClass()));
         buttonPanel.add(new JButton(new CreateNewWorkout()));
         buttonPanel.add(new JButton(new ViewWorkouts()));
         buttonPanel.add(new JButton(new LoadFitnessTracker()));
@@ -145,7 +144,8 @@ public class FitnessTrackerUI extends JFrame {
                 String wkName = workoutName.getText();
                 String wkDate = workoutDate.getText();
                 String wkWeight = workoutWeight.getText();
-                JOptionPane.showMessageDialog(null, "You entered: " + wkName + wkDate + wkWeight);
+                JOptionPane.showMessageDialog(null, "Creating Workout: " + wkName + wkDate + wkWeight);
+                newWorkoutPanel.setVisible(false);
                 Workout newWorkout = new Workout(wkName, wkDate, Double.parseDouble(wkWeight));
                 try {
                     ft.addWorkout(newWorkout);
@@ -163,20 +163,20 @@ public class FitnessTrackerUI extends JFrame {
             newWorkoutPanel.add(workoutWeight);
             newWorkoutPanel.add(workoutName);
             newWorkoutPanel.add(submitButton);
-
+            newWorkoutPanel.setLocation(0,300);
 
             newWorkoutPanel.setLayout(null);
-
-
 
         }
     }
 
 
 
-    // MODIFIES: this
+        // MODIFIES: this
     // EFFECTS: shows list of workouts in GUI, showing the name and date for each workout
     public class ViewWorkouts extends AbstractAction {
+
+
 
         // MODIFIES: this
         // EFFECTS: Creates button for viewing Workouts
@@ -185,25 +185,93 @@ public class FitnessTrackerUI extends JFrame {
         }
 
 
+        // MODIFIES: desktop, workoutListFrame
+        // EFFECTS:  updates and creates WorkoutListFrame and adds to desktop panel
+        public void createWorkoutListFrame() {
+            desktop.remove(workoutListFrame);
+            workoutListFrame = new JInternalFrame("Workout List", false, false, false);
+            desktop.add(workoutListFrame);
+            workoutListFrame.setBackground(Color.gray);
+            workoutListFrame.setPreferredSize(new java.awt.Dimension(300, 300));
+            workoutListFrame.pack();
+            workoutListFrame.setLocation(200, 0);
+            workoutListFrame.setBackground(Color.gray);
+            workoutListFrame.setVisible(true);
+
+        }
+
+
         // Modifies: this
         // EFFECTS: adds all workouts in list to JList and sets the workoutList frame visible. on button click
         @Override
         public void actionPerformed(ActionEvent event) {
-            DefaultListModel<String> model = new DefaultListModel<>();
-            JList<String> workoutJList = new JList<>(model);
+            // Creates panel for WorkoutList
+            createWorkoutListFrame();
 
+            model = new DefaultListModel<>();
+            workoutJList = new JList<>(model);
+
+            // updates workoutList
             List<Workout> workoutList = ft.getWorkoutList();
-            for (Workout wk : workoutList) {
-                model.addElement(wk.getName() + " - " + wk.getDate());
+            for (int i = 0; i < workoutList.size(); i++) {
+                model.addElement(i + ") " + ft.getWorkoutList().get(i).getName() + " - "
+                        + ft.getWorkoutList().get(i).getDate());
             }
 
+
+            // Adds workoutList Frame to Swing Panel
             workoutListFrame.add(workoutJList);
             workoutJList.setLayoutOrientation(JList.VERTICAL);
             workoutJList.setVisible(true);
-            workoutJList.ensureIndexIsVisible(4); // always shows 4 indexes???
             workoutListFrame.setVisible(true);
+
+            // Creates select workout button
+            JButton selectedButton = new JButton(new SelectWorkoutButton());
+            selectedButton.setVisible(true);
+            workoutListFrame.add(selectedButton, BorderLayout.SOUTH);
+
+
+
         }
     }
+
+    // MODIFIES: this
+    // EFFECTS: selects workout from workout list and opens up workout menu for use to edit the workout
+    public class SelectWorkoutButton extends AbstractAction {
+
+        // MODIFIES: this
+        // EFFECTS:    creates button with specified name
+        SelectWorkoutButton() {
+            super("Select to edit or view");
+
+        }
+
+        // MODIFIES: ft
+        // EFFECTS: on button click performs x action
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            String selectedItem = workoutJList.getSelectedValue();
+            JOptionPane.showMessageDialog(null, "You selected: "  + selectedItem);
+            workoutListFrame.setVisible(false);
+            workoutJList.setVisible(false);
+            if (selectedItem != null) {
+                char indexChar = selectedItem.charAt(0);
+                int index = indexChar - '0';
+                Workout selectedWorkout = ft.getWorkoutList().get(index);
+                openWorkoutMenu(selectedWorkout);
+            }
+        }
+    }
+
+
+    // EFFECTS: Opens Workout menu for the selected workout, has buttons to add, edit, view, remove, and save exercises
+    //          to the workout, and to save workout to fitness tracker.
+    public void openWorkoutMenu(Workout wk) {
+
+    }
+
+
+
 
     // MODIFIES: ft
     // EFFECTS: loads in previous FitnessTracker JSon file into ft
