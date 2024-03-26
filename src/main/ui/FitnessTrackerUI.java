fcd0239cea332aa6f0a7e5641a3de25ee9d62767
Palmer Currie <pcurri01@students.cs.ubc.ca>
2024-03-26 13:26:54 -0700

    Added Visual Component, Graph of weight data for latest 10 workouts in FitnessTracker,  Updated documentation for all classes and methods.

--- src/main/ui/FitnessTrackerUI.java
+++ src/main/ui/FitnessTrackerUI.java
@@ -9,10 +9,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-// Represents the applications ui, its main window frame.
+// Represents the FitnessTracker ui, its main window frame (desktop).
 public class FitnessTrackerUI extends JFrame {
 
     private static final int WIDTH = 1080;
@@ -21,6 +24,7 @@ public class FitnessTrackerUI extends JFrame {
     private JDesktopPane desktop;
     private JInternalFrame controlPanel;
     private JInternalFrame workoutListFrame;
+    private JFrame graphFrame;
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
     private static final String JSON_STORE = "./data/fitnesstracker.json";
@@ -28,6 +32,8 @@ public class FitnessTrackerUI extends JFrame {
     private JList<String> workoutJList = new JList<>(model);
 
     // Constructor sets up panel
+    // MODIFIES: this, desktop
+    // EFFECTS:  Creates UI, adds all components to main frame desktop
     public FitnessTrackerUI() {
         ft = new FitnessTracker();
         jsonWriter = new JsonWriter(JSON_STORE);
@@ -38,46 +44,139 @@ public class FitnessTrackerUI extends JFrame {
         // creates the control panel
         controlPanel = new JInternalFrame("Fitness Tracker Panel", true, false, false);
         // For Control Panel
-        controlPanel.setPreferredSize(new java.awt.Dimension(200, 300));
+        controlPanel.setPreferredSize(new Dimension(200, 300));
         controlPanel.pack();
         controlPanel.setVisible(true);
         desktop.add(controlPanel);
 
-
+        // creates workoutListFrame and graphFrame
         workoutListFrame = new JInternalFrame("Workout List", false, false, false);
-
-
-
-
+        graphFrame = new JFrame("Graph");
+        graphFrame.setPreferredSize(new Dimension(500, 500));
 
         setContentPane(desktop);
         setTitle("Fitness Tracker App");
         setSize(WIDTH, HEIGHT);
 
-
         addButtonPanel();
-
-
         setVisible(true);
-
-       // FitnessTrackerUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    something to exit program on X click
 
     }
 
+    // Represents a Graph that will graph out User's Workout Weight data.
+    public class Graph extends JPanel {
+        private static final int MARGIN = 50;
+        private static final int WIDTH = 450;
+        private static final int HEIGHT = 400;
+        private List<Double> weightList = null;
+
+
+        // MODIFIES: this
+        // EFFECTS: creates graph frame of preferred size, (WIDTH, HEIGHT)
+        public Graph() {
+            setPreferredSize(new Dimension(WIDTH, HEIGHT));
+            setBackground(Color.BLACK);
+        }
+
+        // MODIFIES: graphFrame
+        // EFFECTS: paints component (data) onto graph frame
+        @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
+        @Override
+        protected void paintComponent(Graphics graphic) {
+            super.paintComponent(graphic);
+            Graphics2D graphic2D = (Graphics2D) graphic;
+
+            // Draw the x-axis, y-axis and Labels
+            graphic2D.setColor(Color.WHITE);
+            graphic2D.drawLine(MARGIN, HEIGHT - MARGIN, WIDTH - MARGIN, HEIGHT - MARGIN);   // x-axis
+            graphic2D.drawLine(MARGIN, HEIGHT - MARGIN, MARGIN, MARGIN);  // y-axis
+            graphic2D.drawString("Weight Progress of Last 10 Workouts", (WIDTH / 2) - 100, 20);
+            graphic2D.drawString("Workouts (Oldest to Newest)", (WIDTH / 2) - 75, HEIGHT - 10);
+            graphic2D.drawString("Weight", 5, HEIGHT / 2);
+
+
+            // Get data and Max height
+            weightList = getWeightData();
+            Double maxHeight = getMaxHeight(weightList);
+
+            // Distance for separating each point
+            int pointSeparatorX = (WIDTH - 2 * MARGIN) / (weightList.size() - 1);
+            int pointSeparatorY = (int) ((HEIGHT - 2 * MARGIN) / maxHeight);
+
+            // Draw each point and connect with line, label each point with weight text
+            for (int p = 0; p < weightList.size() - 1; p++) {
+                int x1 = (int) (MARGIN + p * pointSeparatorX);
+                int y1 = (int) (HEIGHT - MARGIN - weightList.get(p) * pointSeparatorY);
+                int x2 = MARGIN + (p + 1) * pointSeparatorX;
+                int y2 = (int) (HEIGHT - MARGIN - weightList.get(p + 1) * pointSeparatorY);
+
+
+                graphic2D.setColor(Color.WHITE);
+                graphic2D.drawLine(x1, y1, x2, y2); // Connects point 1 and 2 with line
+                graphic2D.drawString(String.valueOf(weightList.get(p)), x1 - 2, y1 - 15);
+                graphic2D.drawString(String.valueOf(weightList.get(p + 1)), x2 - 2, y2 - 15);
+
+                graphic2D.setColor(Color.BLUE);
+                graphic2D.fillOval(x1 - 2, y1 - 5, 10, 10);  // Draws the point
+                graphic2D.fillOval(x2 - 2, y2 - 5, 10, 10);  // Draws the point
+
+
+            }
+
+
+        }
+
+        // EFFECTS: returns the max point in data, to set upper limit for data points
+        public double getMaxHeight(List<Double> weightList) {
+            double maxHeight = Double.MIN_VALUE;
+            for (double d : weightList) {
+                if (d > maxHeight) {
+                    maxHeight = d;
+                }
+            }
+            return maxHeight;
+        }
+
+        // EFFECTS: returns a list of the users latest 10 workout weights.
+        public List<Double> getWeightData() {
+            List<Double> weightData = new ArrayList<>();
+            if (ft.getListSize() > 10) {
+                for (int i = (ft.getListSize() - 1); i >= (ft.getListSize() - 10); i--) {
+                    Workout wk = ft.getWorkoutList().get(i);
+                    weightData.add(wk.getWeight());
+                }
+            } else {
+                for (int i = (ft.getListSize() - 1); i >= 0; i--) {
+                    Workout wk = ft.getWorkoutList().get(i);
+                    weightData.add(wk.getWeight());
+                }
+            }
+
+            Collections.reverse(weightData);
+            return weightData;
+        }
+
+
+    }
 
 
     // Helper method that adds button panel
     public void addButtonPanel() {
         JPanel buttonPanel = new JPanel();
-        //buttonPanel.add(new JButton(new HelperButtonClass()));
+        //buttonPanel.add(new JButton(new HelperButtonClass())); // TODO: REMOVE ON FINAL COMMIT
+
         buttonPanel.add(new JButton(new CreateNewWorkout()));
         buttonPanel.add(new JButton(new ViewWorkouts()));
+        buttonPanel.add(new JButton(new ViewWeightProgress()));
         buttonPanel.add(new JButton(new LoadFitnessTracker()));
+        buttonPanel.add(new JButton(new SaveFitnessTracker()));
+        buttonPanel.add(new JButton(new ExitWithoutSaving()));
 
         controlPanel.add(buttonPanel);
 
     }
 
+    // TODO:  REMOVE ON FINAL COMMIT
     // MODIFIES: this
     // EFFECTS: what does button do on button click
     public class HelperButtonClass extends AbstractAction {
@@ -96,8 +195,33 @@ public class FitnessTrackerUI extends JFrame {
         }
     }
 
-    // MODIFIES: this
-    // EFFECTS: what does button do on button click
+    // Represents a button that will display User's weight progress on click
+    public class ViewWeightProgress extends AbstractAction {
+
+        // MODIFIES: this
+        // EFFECTS:  creates button with given Name
+        ViewWeightProgress() {
+            super("View Weight Progress");
+        }
+
+        // MODIFIES:  this or ft
+        // EFFECTS: on button click creates Graph and displays it
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            graphFrame.getContentPane().add(new Graph());
+            graphFrame.pack();
+
+            Graph graph = new Graph();
+            graphFrame.add(graph);
+
+            graph.setVisible(true);
+            graphFrame.setVisible(true);
+
+
+        }
+    }
+
+    // Represents a button that will open a create new Workout menu on click
     public class CreateNewWorkout extends AbstractAction {
 
         // MODIFIES: this
@@ -144,7 +268,8 @@ public class FitnessTrackerUI extends JFrame {
                 String wkName = workoutName.getText();
                 String wkDate = workoutDate.getText();
                 String wkWeight = workoutWeight.getText();
-                JOptionPane.showMessageDialog(null, "Creating Workout: " + wkName + wkDate + wkWeight);
+                JOptionPane.showMessageDialog(null, "Creating Workout: " + wkName + " "
+                        + wkDate + " " + wkWeight);
                 newWorkoutPanel.setVisible(false);
                 Workout newWorkout = new Workout(wkName, wkDate, Double.parseDouble(wkWeight));
                 try {
@@ -163,7 +288,7 @@ public class FitnessTrackerUI extends JFrame {
             newWorkoutPanel.add(workoutWeight);
             newWorkoutPanel.add(workoutName);
             newWorkoutPanel.add(submitButton);
-            newWorkoutPanel.setLocation(0,300);
+            newWorkoutPanel.setLocation(0, 300);
 
             newWorkoutPanel.setLayout(null);
 
@@ -171,11 +296,9 @@ public class FitnessTrackerUI extends JFrame {
     }
 
 
-
-        // MODIFIES: this
-    // EFFECTS: shows list of workouts in GUI, showing the name and date for each workout
+    // Represents a button that will show list of workouts in GUI, showing the name and date for each workout
     public class ViewWorkouts extends AbstractAction {
-
+        JPanel workoutButtonPanel;
 
 
         // MODIFIES: this
@@ -198,10 +321,14 @@ public class FitnessTrackerUI extends JFrame {
             workoutListFrame.setBackground(Color.gray);
             workoutListFrame.setVisible(true);
 
+            workoutButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            workoutButtonPanel.setLayout(new BoxLayout(workoutButtonPanel, BoxLayout.Y_AXIS));
+            workoutButtonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
         }
 
 
-        // Modifies: this
+        // MODIFIES: this
         // EFFECTS: adds all workouts in list to JList and sets the workoutList frame visible. on button click
         @Override
         public void actionPerformed(ActionEvent event) {
@@ -224,34 +351,45 @@ public class FitnessTrackerUI extends JFrame {
             workoutJList.setLayoutOrientation(JList.VERTICAL);
             workoutJList.setVisible(true);
             workoutListFrame.setVisible(true);
+            workoutButtonPanel.setVisible(true);
+            workoutListFrame.add(workoutButtonPanel, BorderLayout.SOUTH);
 
             // Creates select workout button
             JButton selectedButton = new JButton(new SelectWorkoutButton());
             selectedButton.setVisible(true);
-            workoutListFrame.add(selectedButton, BorderLayout.SOUTH);
+            workoutButtonPanel.add(selectedButton);
 
+            // Creates remove workout button
+            JButton removeWorkoutButton = new JButton(new RemoveWorkoutButton());
+            removeWorkoutButton.setVisible(true);
+            workoutButtonPanel.add(removeWorkoutButton);
+
+            // Creates reorder workouts button
+            JButton reorderWorkoutsButton = new JButton(new ReorderWorkoutsButton());
+            reorderWorkoutsButton.setVisible(true);
+            workoutButtonPanel.add(reorderWorkoutsButton);
 
 
         }
     }
 
-    // MODIFIES: this
-    // EFFECTS: selects workout from workout list and opens up workout menu for use to edit the workout
+    // Represents a button that will select workout from workout list and
+    //      opens up workout menu for use to edit the workout
     public class SelectWorkoutButton extends AbstractAction {
 
         // MODIFIES: this
         // EFFECTS:    creates button with specified name
         SelectWorkoutButton() {
-            super("Select to edit or view");
+            super("Select Workout");
 
         }
 
         // MODIFIES: ft
-        // EFFECTS: on button click performs x action
+        // EFFECTS: on button click selects the workout that is selected by user
         @Override
         public void actionPerformed(ActionEvent event) {
             String selectedItem = workoutJList.getSelectedValue();
-            JOptionPane.showMessageDialog(null, "You selected: "  + selectedItem);
+            JOptionPane.showMessageDialog(null, "You selected: " + selectedItem);
             workoutListFrame.setVisible(false);
             workoutJList.setVisible(false);
             if (selectedItem != null) {
@@ -264,18 +402,68 @@ public class FitnessTrackerUI extends JFrame {
     }
 
 
+    // TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     // EFFECTS: Opens Workout menu for the selected workout, has buttons to add, edit, view, remove, and save exercises
     //          to the workout, and to save workout to fitness tracker.
     public void openWorkoutMenu(Workout wk) {
-
+        // TODO
     }
 
 
+    // Represents a button that will select workout from workout list and remove that selected workout
+    public class RemoveWorkoutButton extends AbstractAction {
+
+        // MODIFIES: this
+        // EFFECTS:    creates button with specified name
+        RemoveWorkoutButton() {
+            super("Remove Workout");
+
+        }
+
+        // MODIFIES: ft, workoutList
+        // EFFECTS: on button click removes the selected workout from ft and workoutList
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            String selectedItem = workoutJList.getSelectedValue();
+            JOptionPane.showMessageDialog(null, "You Removed: " + selectedItem);
+            workoutListFrame.setVisible(false);
+            workoutJList.setVisible(false);
+            if (selectedItem != null) {
+                char indexChar = selectedItem.charAt(0);
+                int index = indexChar - '0';
+                List<Workout> workoutList = ft.getWorkoutList();
+                Workout selectedWorkout = workoutList.get(index);
+                workoutList.remove(selectedWorkout);
+            }
+        }
+    }
+
+    // Represents a button that will reorder the Workout list in Descending Order, (Newest -> Oldest)
+    public class ReorderWorkoutsButton extends AbstractAction {
+
+        // MODIFIES: this
+        // EFFECTS:  creates button with specified name
+        ReorderWorkoutsButton() {
+            super("Reorder in Descending Order");
+
+        }
+
+        // MODIFIES: ft
+        // EFFECTS: on button click updates the displayed workoutList to be in reverse order
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            model.removeAllElements();
+            // updates workoutList
+            List<Workout> workoutList = ft.getWorkoutList();
+            for (int i = (workoutList.size() - 1); i >= 0; i--) {
+                model.addElement(i + ") " + ft.getWorkoutList().get(i).getName() + " - "
+                        + ft.getWorkoutList().get(i).getDate());
+            }
+        }
+    }
 
 
-    // MODIFIES: ft
-    // EFFECTS: loads in previous FitnessTracker JSon file into ft
-    //          on button click
+    // Represents a button that will load in FitnessTracker data on click
     public class LoadFitnessTracker extends AbstractAction {
 
         // MODIFIES: this
@@ -307,24 +495,89 @@ public class FitnessTrackerUI extends JFrame {
     }
 
 
+    //  Represents a button that will save FitnessTracker data to file on click
+    public class SaveFitnessTracker extends AbstractAction {
+
+        // MODIFIES: this
+        // EFFECTS: Creates button for loading previous fitness tracker
+        SaveFitnessTracker() {
+            super("Save Fitness Tracker");
+        }
+
+        // Modifies: ft
+        // EFFECTS: loads previously saved fitness tracker from json file into ft on button click
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            saveApplication();
+
+        }
+
+        // MODIFIES: jsonWriter
+        // EFFECTS: writes application to file
+        public void saveApplication() {
+            try {
+                jsonWriter.open();
+                jsonWriter.write(ft);
+                jsonWriter.close();
+                System.out.println("Saved Fitness Tracker Application");
+            } catch (FileNotFoundException e) {
+                System.out.println("Unable to write to file...: " + JSON_STORE);
+            }
+        }
+
+    }
+
+    //  Represents a button that will exit the FitnessTracker without saving
+    public class ExitWithoutSaving extends AbstractAction {
+
+        // MODIFIES: this
+        // EFFECTS: Creates button for exiting FitnessTracker without saving
+        ExitWithoutSaving() {
+            super("Exit Without Saving");
+        }
+
+        // EFFECTS: on click closes FitnessTracker without saving
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            System.exit(0);
+        }
+
+    }
+
+
+
 
 
     /* BUTTONS:   (FitnessTrackerMenu)
     -----* Create new Workout
     * Edit Workout
-    * Remove Workout
+    -----*  Remove Workout
     -----* View Workouts
     -----* Load/Reload Previous data
-    * Save and Exit application
-    * Exit Application without saving
+    -----* Save and Exit application
+    -----* Exit Application without saving
     *
      */
 
+    /*
+    TODO:
+       - ------ add in Save application feature / button
+       - ------ remove workout from workout list
+       - ------ re order list from top down
+       - ------ add in visual component  (graph of weight and date?????)
+       - documentation for grader
+       - make sure all class / method / constructor have documentation
+       - add in add exercise and view exercises  (do this last if i have time)
 
+       2 Actions:
+       DONE - remove a workout from workout List
+       DONE - re order the list from top down
+     */
 
 
     // Starts the application
     public static void main(String[] args) {
         new FitnessTrackerUI();
     }
+
 }
